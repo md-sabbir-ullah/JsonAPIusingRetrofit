e2ebe440aa494cb7ea09441a1f4b56d53078a3e0
DESKTOP-NB4VQMM\USER <mullah152144@bscse.uiu.ac.bd>
2020-05-03 00:14:26 +0600

    ADD comment-activity

--- app/src/main/java/esenteq/project/retrofittest/MainActivity.java
+++ app/src/main/java/esenteq/project/retrofittest/MainActivity.java
@@ -2,7 +2,10 @@ package esenteq.project.retrofittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -16,12 +19,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvResult;
+    private Button btnGotoComment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tvResult = findViewById(R.id.tv_resultID);
+        btnGotoComment = findViewById(R.id.btn_gotoCommentID);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://jsonplaceholder.typicode.com/")
@@ -52,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
                 tvResult.setText("Response Failure: " + t.getMessage());
+            }
+        });
+
+        btnGotoComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CommentActivity.class));
+                finish();
             }
         });
     }

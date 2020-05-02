a3149e297e736c66a8aa31f262f902c09a0481cf
DESKTOP-NB4VQMM\USER <mullah152144@bscse.uiu.ac.bd>
2020-05-03 02:19:40 +0600

    ADD addpost-activity

---
+++ app/src/main/java/esenteq/project/retrofittest/AddpostActivity.java
@@ -0,0 +1,14 @@
+package esenteq.project.retrofittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddpostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addpost);
+    }
+}
